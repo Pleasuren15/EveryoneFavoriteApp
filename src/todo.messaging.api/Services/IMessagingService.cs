8358779978f91cbb4b7359835ed6dd3db3f109df
Pleasure Ndhlovu <pleasuren15@gmail.com>
2026-06-25 09:10:49 +0200

    deployment trigger

--- src/todo.messaging.api/Services/IMessagingService.cs
+++ src/todo.messaging.api/Services/IMessagingService.cs
@@ -5,5 +5,10 @@ namespace todo.messaging.api.Services;
 
 public interface IMessagingService
 {
+    /// <summary>
+    /// Sends a message to the user with the expiring tasks.
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <returns></returns>
     Task<ResendResponse> SendExpiringTasks(Guid userId);
 }
